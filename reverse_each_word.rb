cc1b7fec8dc4969b4377cdf5fbc7f28948dd66c0
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 17:01:04 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
-def reverse_each_word(array)
-
+def reverse_each_word(string)
+  new_array = string.split
+  reversed_words = new_array.collect do |word|
+    word.reverse
+  end
+  reversed_words.join(" ")
 end
