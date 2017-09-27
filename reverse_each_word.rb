267f88f5353ce1e9276b4352071472223a669280
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 17:00:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = string.split
   reversed_words = new_array.collect do |word|
-    print word.reverse
+    word.reverse
   end
   reversed_words.join(" ")
 end
